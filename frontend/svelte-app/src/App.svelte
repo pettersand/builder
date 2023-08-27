e26ebfc09c31d377973e21ad464ac0876639a35a
pettersand <petter.sand@gmail.com>
2023-08-27 15:52:03 +0200

    fixed layout

--- frontend/svelte-app/src/App.svelte
+++ frontend/svelte-app/src/App.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+	import Layout from './components/Layout.svelte';
 	export let name: string;
 </script>
-
+  
+<Layout title="My Svelte App">
 <h1>Hello, {name}!</h1>
+</Layout>
+
+<style>
+/* Add your App-specific styles here */
+</style>
--- frontend/svelte-app/src/components/Layout.svelte
+++ frontend/svelte-app/src/components/Layout.svelte
@@ -1,32 +1,28 @@
+<!-- Layout.svelte -->
 <script lang="ts">
-    
+  export let title: string;
 </script>
-  
-  <style>
-    
-    
-    .header {
-      @apply bg-blue-500 text-white p-4;
-    }
-  
-    .footer {
-      @apply bg-gray-800 text-white p-4;
-    }
-  </style>
-  
-  <div class="layout">
-    <header class="header">
-      <h1>My App</h1>
-    </header>
-  
-    <main>
-      <slot></slot>
-    </main>
-  
-    <footer class="footer">
-      <p>Footer Content</p>
-    </footer>
-  </div>
-  
 
-  
+<style>
+  .header {
+    @apply bg-blue-500 text-white p-4;
+  }
+
+  .footer {
+    @apply bg-gray-800 text-white p-4;
+  }
+</style>
+
+<div class="layout">
+  <header class="header">
+    <h1>{title}</h1> <!-- Include the title prop here -->
+  </header>
+
+  <main>
+    <slot></slot>
+  </main>
+
+  <footer class="footer">
+    <p>Footer Content</p>
+  </footer>
+</div>
