875f822f05321dd34f97a9656fc7560eb2ac681c
pettersand <petter.sand@gmail.com>
2023-09-09 22:37:35 +0200

    first draft frontend login/reg done, starting backend

--- frontend/svelte-app/src/components/LoginRegisterModal.svelte
+++ frontend/svelte-app/src/components/LoginRegisterModal.svelte
@@ -7,6 +7,10 @@
     handleKeyboardEvent,
     handleClickOutside,
   } from "../utilities/modalUtilities";
+  import {
+    emailPattern,
+    passwordPattern,
+  } from "../utilities/validationPatterns";
 
   let modalRef: HTMLElement;
   let modalBox: HTMLElement;
@@ -25,6 +29,37 @@
   let gender = "";
   let biologicalSex = "";
   let agreeToTerms = false;
+
+  let usernameTyped = false;
+  let emailTyped = false;
+  let passwordTyped = false;
+  let confirmPasswordTyped = false;
+  let usernameValid = false;
+  let emailValid = false;
+  let passwordValid = false;
+  let confirmPasswordValid = false;
+  let passwordsMatch = false;
+
+  function checkValidity(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.name === "username") {
+      usernameValid = input.checkValidity();
+      usernameTyped = true;
+    } else if (input.name === "email") {
+      emailValid = input.checkValidity();
+      emailTyped = true;
+    } else if (input.name === "password") {
+      passwordValid = input.checkValidity();
+      passwordTyped = true;
+    } else if (input.name === "confirmPassword") {
+      confirmPasswordValid = input.checkValidity();
+      confirmPasswordTyped = true;
+      const passwordInput = document.getElementById(
+        "password"
+      ) as HTMLInputElement;
+      passwordsMatch = passwordInput.value === input.value;
+    }
+  }
 
   // TODO: Add form submission logic
 
@@ -55,6 +90,16 @@
       window.removeEventListener("keydown", handleKey);
     };
   });
+
+  let step = 1; // To track the current step of registration
+
+  const goToNextStep = () => {
+    step++;
+  };
+
+  const goToPreviousStep = () => {
+    step--;
+  };
 </script>
 
 {#if $modalStore.showModal}
@@ -65,39 +110,277 @@
     bind:this={modalRef}
   >
     <div
-      class={`auth-modal p-6 w-1/2 rounded-lg ${
+      class={`flex flex-row p-6 w-1/2 rounded-lg ${
         $themeStore === "dark"
           ? "bg-dark-card text-dark-text"
           : "bg-light-card text-light-text"
       }`}
       bind:this={modalBox}
     >
-      <button class="absolute top-0 right-0 p-2 rounded-full"> X </button>
-      <div class="login-section">
+      <button
+        class="absolute top-0 right-0 p-2 rounded-full"
+        on:click={() => modalStore.closeModal()}
+      >
+        X
+      </button>
+      <div class="login-section flex flex-col w-1/2 border-r-2 border-black">
+        <h2>Login</h2>
         <!-- Login Fields -->
       </div>
-      <div class="register-section">
-        <!-- Register Fields -->
-      </div>
-      <div class="mt-4">
-        <button
-          class={`px-4 py-2 mr-2 rounded-full ${
+      <div class="register-section flex flex-col w-1/2 items-center">
+        {#if step === 1}
+          <h2 class="text-center w-full p-2">Register</h2>
+          <div class="w-full p-4">
+            <label for="username" class="block text-sm font-medium"
+              >Username</label
+            >
+            <input
+              id="username"
+              name="username"
+              type="text"
+              placeholder="Username"
+              required
+              minlength="3"
+              maxlength="20"
+              on:input={checkValidity}
+              class={`w-full p-2 mb-2 rounded border ${
+                $themeStore === "dark"
+                  ? "bg-dark-input text-dark-text"
+                  : "bg-light-input text-light-text"
+              } ${usernameValid ? "" : "border-red-500"}`}
+            />
+            {#if !usernameValid && usernameTyped}
+              <small class="text-red-500">Username is not valid</small>
+            {/if}
+            <label for="email" class="block text-sm font-medium">Email</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Email"
+              required
+              pattern={emailPattern}
+              on:input={checkValidity}
+              class={`w-full p-2 mb-2 rounded border ${
+                $themeStore === "dark"
+                  ? "bg-dark-input text-dark-text"
+                  : "bg-light-input text-light-text"
+              } ${emailValid ? "" : "border-red-500"}`}
+            />
+            {#if !emailValid && emailTyped}
+              <small class="text-red-500">Email is not valid</small>
+            {/if}
+            <label for="password" class="block text-sm font-medium"
+              >Password</label
+            >
+            <input
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Password"
+              required
+              minlength="6"
+              pattern={passwordPattern}
+              on:input={checkValidity}
+              class={`w-full p-2 mb-2 rounded border ${
+                $themeStore === "dark"
+                  ? "bg-dark-input text-dark-text"
+                  : "bg-light-input text-light-text"
+              } ${passwordValid ? "" : "border-red-500"}`}
+            />
+            <small class="text-gray-500"
+              >Must contain an uppercase letter, a lowercase letter, a number,
+              and a special character</small
+            >
+            {#if !passwordValid && passwordTyped}
+              <small class="text-red-500">Password is not valid</small>
+            {/if}
+            <label for="confirmPassword" class="block text-sm font-medium"
+              >Confirm Password</label
+            >
+            <input
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              placeholder="Confirm Password"
+              required
+              minlength="6"
+              pattern={passwordPattern}
+              on:input={checkValidity}
+              class={`w-full p-2 mb-2 rounded border ${
+                $themeStore === "dark"
+                  ? "bg-dark-input text-dark-text"
+                  : "bg-light-input text-light-text"
+              } ${confirmPasswordValid ? "" : "border-red-500"}`}
+            />
+            {#if !confirmPasswordValid && confirmPasswordTyped && passwordsMatch}
+              <small class="text-red-500">Passwords do not match</small>
+            {/if}
+            <button
+              type="submit"
+              on:click={goToNextStep}
+              disabled={!usernameValid ||
+                !emailValid ||
+                !passwordValid ||
+                !confirmPasswordValid ||
+                !passwordsMatch}
+              class={`w-full p-2 rounded ${
+                $themeStore === "dark"
+                  ? "bg-dark-primary text-dark-text"
+                  : "bg-light-primary text-light-text"
+              } ${
+                !usernameValid ||
+                !emailValid ||
+                !passwordValid ||
+                !confirmPasswordValid
+                  ? "opacity-50 cursor-not-allowed"
+                  : ""
+              }`}
+            >
+              Next
+            </button>
+          </div>
+        {:else if step === 2}
+        <h2 class="text-center w-full p-2">Additional Information</h2>
+
+        <!-- First Name -->
+        <label for="firstName" class="block text-sm font-medium">First Name</label>
+        <input
+          id="firstName"
+          name="firstName"
+          type="text"
+          placeholder="First Name"
+          required
+          minlength="2"
+          maxlength="50"
+          class={`w-full p-2 mb-2 rounded ${
             $themeStore === "dark"
-              ? "bg-dark-primary hover:bg-dark-primary2 text-dark-text "
-              : "bg-light-primary hover:bg-light-primary2 text-light-text"
+              ? "bg-dark-input text-dark-text"
+              : "bg-light-input text-light-text"
+          }`}
+        />
+      
+        <!-- Last Name -->
+        <label for="lastName" class="block text-sm font-medium">Last Name</label>
+        <input
+          id="lastName"
+          name="lastName"
+          type="text"
+          placeholder="Last Name"
+          required
+          minlength="2"
+          maxlength="50"
+          class={`w-full p-2 mb-2 rounded ${
+            $themeStore === "dark"
+              ? "bg-dark-input text-dark-text"
+              : "bg-light-input text-light-text"
+          }`}
+        />
+      
+        <!-- Date of Birth -->
+        <label for="dob" class="block text-sm font-medium">Date of Birth</label>
+        <input
+          id="dob"
+          name="dob"
+          type="date"
+          required
+          class={`w-full p-2 mb-2 rounded ${
+            $themeStore === "dark"
+              ? "bg-dark-input text-dark-text"
+              : "bg-light-input text-light-text"
+          }`}
+        />
+      
+        <!-- Country -->
+        <label for="country" class="block text-sm font-medium">Country</label>
+        <input
+          id="country"
+          name="country"
+          type="text"
+          placeholder="Country"
+          required
+          minlength="2"
+          maxlength="50"
+          class={`w-full p-2 mb-2 rounded ${
+            $themeStore === "dark"
+              ? "bg-dark-input text-dark-text"
+              : "bg-light-input text-light-text"
+          }`}
+        />
+      
+        <!-- Gender -->
+        <label for="gender" class="block text-sm font-medium">Gender</label>
+        <input
+          id="gender"
+          name="gender"
+          type="text"
+          placeholder="Gender"
+          class={`w-full p-2 mb-2 rounded ${
+            $themeStore === "dark"
+              ? "bg-dark-input text-dark-text"
+              : "bg-light-input text-light-text"
+          }`}
+        />
+      
+        <!-- Biological Sex -->
+        <label for="bioSex" class="block text-sm font-medium">Biological Sex</label>
+        <input
+          id="bioSex"
+          name="bioSex"
+          type="text"
+          placeholder="Biological Sex"
+          class={`w-full p-2 mb-2 rounded ${
+            $themeStore === "dark"
+              ? "bg-dark-input text-dark-text"
+              : "bg-light-input text-light-text"
+          }`}
+        />
+      
+        <!-- Checkboxes -->
+        <div class="flex flex-col">
+          <label class="inline-flex items-center">
+            <input type="checkbox" class="form-checkbox" name="isTrainer" />
+            <span class="ml-2">I am a trainer</span>
+          </label>
+          <label class="inline-flex items-center">
+            <input type="checkbox" class="form-checkbox" name="hasTrainer" />
+            <span class="ml-2">I have a trainer / I am a client</span>
+          </label>
+          <label class="inline-flex items-center">
+            <input type="checkbox" class="form-checkbox" name="terms" required />
+            <span class="ml-2">I agree to the Terms of Service</span>
+          </label>
+        </div>
+      
+        <!-- Privacy Policy Button -->
+        <button
+          class={`w-full p-2 mt-4 rounded ${
+            $themeStore === "dark"
+              ? "bg-dark-secondary hover:bg-dark-secondary2 text-dark-text"
+              : "bg-light-secondary hover:bg-light-secondary2 text-light-text"
           }`}
         >
-          Confirm
+          Privacy Policy
         </button>
-        <button
-          class={`px-4 py-2 rounded-full ${
-            $themeStore === "dark"
-              ? "bg-dark-primary hover:bg-dark-primary2 text-dark-text "
-              : "bg-light-primary hover:bg-light-primary2 text-light-text"
-          }`}
-        >
-          Exit
-        </button>
+            <button
+              class={`px-4 py-2 mr-2 rounded-full ${
+                $themeStore === "dark"
+                  ? "bg-dark-primary hover:bg-dark-primary2 text-dark-text "
+                  : "bg-light-primary hover:bg-light-primary2 text-light-text"
+              }`}
+              on:click={goToPreviousStep}>Previous</button
+            >
+            <button
+              class={`px-4 py-2 mr-2 rounded-full ${
+                $themeStore === "dark"
+                  ? "bg-dark-primary hover:bg-dark-primary2 text-dark-text "
+                  : "bg-light-primary hover:bg-light-primary2 text-light-text"
+              }`}
+              on:click={() => modalStore.closeModal()}
+            >
+              Register
+            </button>
+        {/if}
       </div>
     </div>
   </div>
---
+++ frontend/svelte-app/src/utilities/validationPatterns.ts
@@ -0,0 +1,4 @@
+// validationPatterns.ts
+
+export const emailPattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$";
+export const passwordPattern = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}";
