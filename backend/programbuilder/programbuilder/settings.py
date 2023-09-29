c8851d259c056c0918a488da05962a150b07d7ea
pettersand <petter.sand@gmail.com>
2023-09-29 19:10:37 +0200

    updated settings.py

--- backend/programbuilder/programbuilder/settings.py
+++ backend/programbuilder/programbuilder/settings.py
@@ -14,7 +14,10 @@ from pathlib import Path
 import os
 from decouple import Config, RepositoryEnv
 
-config = Config(RepositoryEnv(".env"))
+try:
+    config = Config(RepositoryEnv(".env"))
+except FileNotFoundError:
+    config = Config(os.environ)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = config("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
