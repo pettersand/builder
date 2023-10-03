0aaf7bd95cbe6b8a71929541c4a817b3301c0b7a
pettersand <petter.sand@gmail.com>
2023-10-03 19:37:25 +0200

    added template db population script and json

--- backend/programbuilder/builder/management/commands/populate_official_templates.py
+++ backend/programbuilder/builder/management/commands/populate_official_templates.py
@@ -7,15 +7,20 @@ class Command(BaseCommand):
     help = "Populate database with official beginner templates"
 
     def handle(self, *args, **kwargs):
-        with open("path/file.json", "r") as file:
+        with open(
+            "builder/management/files/official_beginner_templates.json", "r"
+        ) as file:
             templates = json.load(file)
             for template in templates:
-                OfficialBeginnerTemplate.objects.create(
-                    name=template["name"],
-                    description=template["description"],
-                    training_days=template["training_days"],
-                    exercises_per_day=template["exercises_per_day"],
-                    sessions=template["sessions"],
-                    blocks=template["blocks"],
-                )
+                if not OfficialBeginnerTemplate.objects.filter(
+                    name=template["name"]
+                ).exists():
+                    OfficialBeginnerTemplate.objects.create(
+                        name=template["name"],
+                        description=template["description"],
+                        training_days=template["training_days"],
+                        exercises_per_day=template["exercises_per_day"],
+                        sessions=template["sessions"],
+                        blocks=template["blocks"],
+                    )
         self.stdout.write(self.style.SUCCESS("Successfully populated database"))
