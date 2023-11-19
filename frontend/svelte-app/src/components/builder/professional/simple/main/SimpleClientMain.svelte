f598a5d1a31f279bcd689b034ae3ff17e16a80d6
pettersand <petter.sand@gmail.com>
2023-11-19 18:09:55 +0100

    design of client data presentation

--- frontend/svelte-app/src/components/builder/professional/simple/main/SimpleClientMain.svelte
+++ frontend/svelte-app/src/components/builder/professional/simple/main/SimpleClientMain.svelte
@@ -15,58 +15,70 @@
 </script>
 
 <div class="gap-4 w-full h-full flex flex-row">
+  <!-- Program History Container -->
   <div
     class="flex flex-grow w-1/3 bg-bg flex flex-col justify-start p-3 shadow-xl"
   >
     <span class="font-bold text-lg">Program History</span>
   </div>
+
+  <!-- Client Data & Notes Container -->
   <div
     class="flex flex-grow w-1/3 bg-bg flex flex-col justify-start p-3 gap-2 shadow-xl"
   >
+    <!-- Client Data Container -->
     <div class="h-1/2 flex flex-col gap-4 custom-border-bottom">
       <span class="font-bold text-lg">Client Data</span>
       {#if clientDetails}
-        <div class="flex flex-col gap-4 justify-evenly">
-          <div>
-            <span class="font-bold">Goals:</span>
-            {#if clientDetails.goals && clientDetails.goals.length}
-              {#each clientDetails.goals as goal}
-                <p>{goal.content} (Due: {goal.goal_date})</p>
-              {/each}
-            {:else}
-              <p>No Goals Data</p>
-            {/if}
-          </div>
-
-          <div>
-            <span class="font-bold">Injuries:</span>
-            {#if clientDetails.injuries && clientDetails.injuries.length}
-              {#each clientDetails.injuries as injury}
-                <p>{injury.injury_name} - {injury.site}</p>
-              {/each}
-            {:else}
-              <p>No Injuries Data</p>
-            {/if}
-          </div>
-
-          <div>
-            <span class="font-bold">Preferences:</span>
-            {#if clientDetails.preferences && clientDetails.preferences.length}
-              {#each clientDetails.preferences as preference}
-                <p>{JSON.stringify(preference.details)}</p>
-              {/each}
-            {:else}
-              <p>No Preferences Data</p>
-            {/if}
-          </div>
-
-          <span>Strength</span>
-          <span>Completion Stats</span>
-          <span>Progress Stats</span>
+        <!-- Goals Container -->
+        <div class="flex flex-col gap-4 justify-evenly custom-border-bottom">
+          <span class="font-bold">Goals:</span>
+          {#if clientDetails.goals && clientDetails.goals.length}
+            {#each clientDetails.goals as goal}
+              <div class="flex flex-row justify-between bg-card">
+                <div class="flex flex-col">
+                  <p>{goal.goal_type} Due: {goal.goal_date}</p>
+                  <p>{goal.content}</p>
+                </div>
+                <div class="flex flex-col">
+                  <p>Time To Goal</p>
+                </div>
+              </div>
+            {/each}
+          {:else}
+            <p>No Goals Data</p>
+          {/if}
         </div>
+        <!-- Injuries Container -->
+        <div class="flex flex-col gap-4 justify-evenly custom-border-bottom">
+          <span class="font-bold">Injuries:</span>
+          {#if clientDetails.injuries && clientDetails.injuries.length}
+            {#each clientDetails.injuries as injury}
+              <p>{injury.injury_name} - {injury.site}</p>
+            {/each}
+          {:else}
+            <p>No Injuries Data</p>
+          {/if}
+        </div>
+
+        <div class="flex flex-col gap-4 justify-evenly">
+          <span class="font-bold">Preferences:</span>
+          {#if clientDetails.preferences && clientDetails.preferences.length}
+            {#each clientDetails.preferences as preference}
+              <p>{JSON.stringify(preference.details)}</p>
+            {/each}
+          {:else}
+            <p>No Preferences Data</p>
+          {/if}
+        </div>
+
+        <span>Strength</span>
+        <span>Completion Stats</span>
+        <span>Progress Stats</span>
       {/if}
     </div>
 
+    <!-- Client Notes Container -->
     <div class="h-1/2 flex flex-col">
       <div>
         <span class="font-bold">Notes:</span>
