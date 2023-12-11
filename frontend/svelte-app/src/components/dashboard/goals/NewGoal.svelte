6e7b2a975dcaabbc5c581199b7d1d5e4e4eae975
pettersand <petter.sand@gmail.com>
2023-12-11 19:54:18 +0100

    added bind:value

--- frontend/svelte-app/src/components/dashboard/goals/NewGoal.svelte
+++ frontend/svelte-app/src/components/dashboard/goals/NewGoal.svelte
@@ -16,19 +16,23 @@
   <div class="flex gap-4 w-full">
     <div class="flex items-center gap-4">
       <label for="date" class="whitespace-nowrap">Set Due Date</label>
-      <input type="date" class="border border-paragraph rounded-lg p-2" />
+      <input
+        type="date"
+        bind:value={longTermGoal.dueDate}
+        class="border border-paragraph rounded-lg p-2"
+      />
     </div>
     <div class="flex flex-col w-full">
       <input
         type="textbox"
         placeholder="Enter Goal"
+        bind:value={longTermGoal.goal}
         class="border border-paragraph rounded-lg p-2"
       />
     </div>
   </div>
   <div class="flex w-full items-center justify-center">
-    <button class="bg-accent2 rounded-lg p-2 font-bold">ADD GOAL</button
-    >
+    <button class="bg-accent2 rounded-lg p-2 font-bold">ADD GOAL</button>
   </div>
 </div>
 
@@ -49,7 +53,6 @@
     </div>
   </div>
   <div class="flex w-full items-center justify-center">
-    <button class="bg-accent2 rounded-lg p-2 font-bold">ADD GOAL</button
-    >
+    <button class="bg-accent2 rounded-lg p-2 font-bold">ADD GOAL</button>
   </div>
 </div>
