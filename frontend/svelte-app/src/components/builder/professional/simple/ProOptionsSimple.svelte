6ca2603361617fc17e5afa88aa62e2378a2ea873
Petter Austnes <petter.sand@gmail.com>
2024-01-08 16:19:35 +0100

    removed bugged ref to FileOptions

--- frontend/svelte-app/src/components/builder/professional/simple/ProOptionsSimple.svelte
+++ frontend/svelte-app/src/components/builder/professional/simple/ProOptionsSimple.svelte
@@ -12,7 +12,18 @@
 </script>
 
 <div class="flex flex-row justify-between">
-  <FileOptions />
+  <div class="flex flex-row items-start custom-border-right p-4">
+    <div
+      class="flex flex-col items-start justify-center whitespace-nowrap gap-2 pr-6"
+    >
+      <span class="font-bold text-lg border-b border-accent2 w-full">File</span>
+      <button class="text-gray-400"> New </button>
+      <button class="text-gray-400"> Save </button>
+      <button class="text-gray-400"> Save As </button>
+      <button class="text-gray-400"> Open </button>
+      <button class="text-gray-400"> Send </button>
+    </div>
+  </div>
   {#if activeStep === "step1"}
     <SimpleClientOptions />
   {:else if activeStep === "step2"}
