e4bb3a0cfce2893eb71f726fe11a4c2b154058a3
Petter Austnes <petter.sand@gmail.com>
2024-01-11 17:59:51 +0100

    add new goal self

--- frontend/svelte-app/src/components/dashboard/goals/NewGoal.svelte
+++ frontend/svelte-app/src/components/dashboard/goals/NewGoal.svelte
@@ -12,47 +12,43 @@
    * TODO: Add error/confirmation handling
    */
 
-  import { saveGoal } from "../../../utilities/goals/index";
-  import { GoalType, type Goal } from "../../../utilities/goals/index";
+  import {
+    saveGoal,
+    goalsStore,
+    determineCreator,
+    GoalType,
+    type Goal,
+  } from "../../../utilities/goals/index";
 
-  let createdBy = "";
-  let createdFor = "";
+/*   let createdBy = "";
+  let createdFor = ""; */
+  
+  //! Temporary until auth request is implemented
+  let tempUserId = "bc7c1679-6a15-47f2-a33d-253b1f286b6f";
 
   let goalForm: Goal = {
+    createdBy: tempUserId,
+    createdFor: tempUserId,
     goal: "",
     dueDate: new Date(),
     type: GoalType.Long,
     private: false,
-  }
+  };
 
-/**
- * TODO: Create function to determine owner of goal (client or self)
- * */ 
-  $: createdFor = determineCreator();
+  /**
+   * TODO: Create function to determine owner of goal (client or self)
+   * */
+/*   $: createdFor = determineCreator(); */
 
   const addGoal = async () => {
-    goalForm.createdBy = createdBy;
-    goalForm.createdFor = createdFor;
-
     try {
       const savedGoal = await saveGoal(goalForm);
-      // TODO: Update store/storage
+      goalsStore.addGoal(savedGoal);
       console.log("New Goal Added", savedGoal);
     } catch (error) {
       console.error("Error creating goal:", error);
     }
-  }; 
-
-
-
-  const updateGoalsStorage = (newGoal: FrontendGoal): void => {
-    const currentGoals: FrontendGoal[] = JSON.parse(
-      sessionStorage.getItem("goals") || "[]"
-    );
-    currentGoals.push(newGoal);
-    sessionStorage.setItem("goals", JSON.stringify(currentGoals));
   };
-
 </script>
 
 <div class="flex flex-col items-start p-2 gap-4 bg-bg custom-border-bottom">
@@ -63,7 +59,7 @@
       <label for="date" class="whitespace-nowrap">Set Due Date</label>
       <input
         type="date"
-        bind:value={longTermGoal.dueDate}
+        bind:value={goalForm.dueDate}
         class="border border-paragraph rounded-lg p-2"
       />
     </div>
@@ -71,7 +67,7 @@
       <input
         type="textbox"
         placeholder="Enter Goal"
-        bind:value={longTermGoal.goal}
+        bind:value={goalForm.goal}
         class="border border-paragraph rounded-lg p-2"
       />
     </div>
@@ -79,33 +75,8 @@
   <div class="flex w-full items-center justify-center">
     <button
       class="bg-accent2 rounded-lg p-2 font-bold"
-      on:click={addLongTermGoal}>ADD GOAL</button
+      on:click={addGoal}>ADD GOAL</button
     >
   </div>
 </div>
 
-<div class="flex flex-col items-start p-2 gap-4 bg-bg custom-border-bottom">
-  <div class="text-lg font-bold">Short Term Goal</div>
-
-  <div class="flex gap-4 w-full">
-    <div class="flex items-center gap-4">
-      <label for="date" class="whitespace-nowrap">Set Due Date</label>
-      <input
-        type="date"
-        bind:value={shortTermGoal.dueDate}
-        class="border border-paragraph rounded-lg p-2"
-      />
-    </div>
-    <div class="flex flex-col w-full">
-      <input
-        type="textbox"
-        placeholder="Enter Goal"
-        bind:value={shortTermGoal.goal}
-        class="border border-paragraph rounded-lg p-2"
-      />
-    </div>
-  </div>
-  <div class="flex w-full items-center justify-center">
-    <button class="bg-accent2 rounded-lg p-2 font-bold">ADD GOAL</button>
-  </div>
-</div>
