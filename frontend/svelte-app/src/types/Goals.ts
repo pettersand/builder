cc17b312ba6a2a9800c4ed056d48aa0e83d31c8f
Petter Austnes <petter.sand@gmail.com>
2024-01-11 15:24:45 +0100

    restructure util file handling

--- frontend/svelte-app/src/types/Goals.ts
+++ frontend/svelte-app/src/types/Goals.ts
@@ -1,54 +1,19 @@
+/**
+ * ! MOVED to goals base utility folder
+ */
+
+export enum GoalType {
+  Long = "long",
+  Short = "short",
+}
+
 export interface BaseGoal {
-  id?: number;
-  type: "long" | "short";
+  id?: string; // Optional for new goals, required for existing ones
+  type: GoalType;
   goal: string;
+  dueDate: Date;
+  createdBy?: string; // User ID of the creator
+  createdFor?: string; // User ID of the goal owner, if different from creator
+  private?: boolean; // Only relevant for clients, not trainers
 }
 
-export interface FrontendGoal extends BaseGoal {
-  dueDate: string;
-  status?: "active" | "completed" | "abandoned";
-  private?: boolean;
-  createdBy?: string;
-  createdFor?: string;
-}
-
-export class Goal implements FrontendGoal {
-  id?: number;
-  dueDate: string;
-  goal: string;
-  type: "long" | "short";
-  status?: "active" | "completed" | "abandoned";
-  private?: boolean;
-  createdBy?: string;
-  createdFor?: string;
-
-  constructor(
-    goal: string,
-    type: "long" | "short",
-    dueDate: string,
-    status?: "active" | "completed" | "abandoned",
-    isPrivate?: boolean,
-    createdBy?: string,
-    createdFor?: string
-  ) {
-    this.goal = goal;
-    this.type = type;
-    this.dueDate = dueDate;
-    this.status = status;
-    this.private = isPrivate;
-    this.createdBy = createdBy;
-    this.createdFor = createdFor;
-  }
-
-  forAPI(): any {
-    return {
-      content: this.goal,
-      type: this.type,
-      goal_date: this.dueDate,
-      status: this.status,
-      private: this.private,
-      created_for: this.createdFor,
-      user_id: this.createdBy,
-    };
-  }
-}
