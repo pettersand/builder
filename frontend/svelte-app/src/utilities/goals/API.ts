b26f837bd299548ef75114432467153764261990
Petter Austnes <petter.sand@gmail.com>
2024-01-11 15:58:56 +0100

    add error handling goal/api

--- frontend/svelte-app/src/utilities/goals/API.ts
+++ frontend/svelte-app/src/utilities/goals/API.ts
@@ -3,38 +3,57 @@
 import api from "../api";
 import type { Goal } from "./index";
 
-export const saveGoal = async (goal: Goal): Promise<Goal> => {
-
-    const method = goal.id ? "put" : "post";
-    const endpoint = goal.id ? `/goals/${goal.id}` : "/goals";
-
-    const response = await api[method](endpoint, goal);
-    return response.data;
-}
-
-export const newLongTermGoal = async (goalData: Goal): Promise<any> => {
-    try {
-      const response = await api.post("/new_simple_goal/", goalData);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
+const convertToBackend = (goal: Goal): any => {
+  return {
+    ...goal,
+    goal_date: goal.dueDate,
+    content: goal.goal,
+    user_id: goal.createdBy,
+    created_for: goal.createdFor,
   };
-  
-  export const getGoals = async (): Promise<any> => {
-    try { 
-      const response = await api.get("/get_goals/");
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  }; 
-  
-  export const deleteUserGoal = async (goalId: number): Promise<any> => {
-    try {
-      const response = await api.delete(`/delete_goal/${goalId}`);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
+};
+
+const convertToFrontend = (data: any): Goal => {
+  return {
+    ...data,
+    dueDate: data.goal_date,
+    goal: data.content,
+    createdBy: data.user_id,
+    createdFor: data.created_for,
+  };
+};
+
+export const saveGoal = async (goal: Goal): Promise<Goal> => {
+  try {
+    const backendGoal = convertToBackend(goal);
+    const method = goal.id ? "put" : "post";
+    const endpoint = goal.id
+      ? `/update_simple_goal/${goal.id}`
+      : "/new_simple_goal/";
+
+    const response = await api[method](endpoint, backendGoal);
+    return convertToFrontend(response.data);
+  } catch (error) {
+    console.error("Error saving goal", error);
+    throw new Error("Error saving goal.");
   }
+};
+
+export const getGoals = async (): Promise<Goal[]> => {
+  try {
+    const response = await api.get("/get_goals/");
+    return response.data.map(convertToFrontend);
+  } catch (error) {
+    console.error("Error getting goals:", error);
+    throw new Error('Error getting goals.');
+  }
+};
+
+export const deleteUserGoal = async (goalId: number): Promise<void> => {
+  try {
+    const response = await api.delete(`/delete_goal/${goalId}`);
+  } catch (error) {
+    console.error("Error deleting goal:", error);
+    throw new Error('Error deleting goal.');
+  }
+};
