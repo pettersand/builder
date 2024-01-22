767d860a7130856888fc3bd6280464c17620075f
Petter Austnes <petter.sand@gmail.com>
2024-01-22 19:06:41 +0100

    add methods to feedbackStore

--- frontend/svelte-app/src/utilities/feedback/store.ts
+++ frontend/svelte-app/src/utilities/feedback/store.ts
@@ -1,10 +1,15 @@
 /**
  * TODO: Store to manage state of all feedback types. Visibility, content, and type of feedback.
- * TODO: Define clear guidelines for responses, and how to handle usage in other components. 
+ * TODO: Define clear guidelines for responses, and how to handle usage in other components.
  */
 
 import { subscribe } from "svelte/internal";
 import { writable } from "svelte/store";
+import type {
+  ErrorMessage,
+  ConfirmationMessage,
+  SuccessMessage,
+} from "./types";
 
 //* Error state - errorMessage, errorCode, errorDetails
 //* Confirmation state - confirmationMessage, onConfirm, onCancel
@@ -24,13 +29,69 @@ const defaultState = {
 };
 
 const createFeedbackStore = () => {
-   const { subscribe, set, update } = feedbackStore
+  const { subscribe, set, update } = writable(defaultState);
 
-    return {
-        subscribe, 
-        set,
-        update,
-    };
+  const setError = (error: ErrorMessage) => {
+    update((state) => ({
+      ...state,
+      errorMessage: error.errorMessage,
+      errorCode: error.errorCode,
+      errorDetails: error.errorDetails,
+    }));
+  };
+
+  const clearError = () => {
+    update((state) => ({
+      ...state,
+      errorMessage: "",
+      errorCode: "",
+      errorDetails: "",
+    }));
+  };
+
+  const setConfirmation = (confirmation: ConfirmationMessage) => {
+    update((state) => ({
+      ...state,
+      confirmationMessage: confirmation.confirmationMessage,
+      onConfirm: confirmation.onConfirm,
+      onCancel: confirmation.onCancel,
+    }));
+  };
+
+  const clearConfirmation = () => {
+    update((state) => ({
+      ...state,
+      confirmationMessage: "",
+      onConfirm: () => {},
+      onCancel: () => {},
+    }));
+  };
+
+  const setSuccess = (success: SuccessMessage) => {
+    update((state) => ({
+      ...state,
+      successMessage: success.successMessage,
+    }));
+  };
+
+  const clearSuccess = () => {
+    update((state) => ({
+      ...state,
+      successMessage: "",
+    }));
+  };
+
+
+
+  return {
+    subscribe,
+    setError,
+    clearError,
+    setConfirmation,
+    clearConfirmation,
+    setSuccess,
+    clearSuccess,
+  };
 };
 
 export const feedbackStore = createFeedbackStore();
--- frontend/svelte-app/src/utilities/feedback/types.ts
+++ frontend/svelte-app/src/utilities/feedback/types.ts
@@ -1,3 +1,20 @@
 /**
  * TODO: Types for messages, confirmation dialogs, etc
  */
+
+export interface ErrorMessage {
+    errorMessage: string;
+    errorCode: string;
+    errorDetails: string;
+}
+
+export interface ConfirmationMessage {
+    confirmationMessage: string;
+    onConfirm: () => void;
+    onCancel: () => void;
+}
+
+export interface SuccessMessage {
+    successMessage: string;
+}
+
