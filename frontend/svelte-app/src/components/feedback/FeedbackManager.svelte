4a7ee9d9623889aa576a292bd0b04080f2eb2b69
Petter Austnes <petter.sand@gmail.com>
2024-01-22 19:52:56 +0100

    add new ErrorModal popup

--- frontend/svelte-app/src/components/feedback/FeedbackManager.svelte
+++ frontend/svelte-app/src/components/feedback/FeedbackManager.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-    import { feedbackStore } from "../../utilities/feedback/store";
-    import Toast from "./Toast.svelte";
-    
+  import { feedbackStore } from "../../utilities/feedback/store";
+  import ErrorModal from "./ErrorModal.svelte";
+  import Toast from "./Toast.svelte";
 </script>
 
 <div class="fixed top-0 left-0 w-full h-full z-50">
-    <div class="absolute bottom-5 left-5">
-        <Toast />
-    </div>
-
+  <div class="absolute top-12 left-2/4">
+    <ErrorModal />
+  </div>
+  <div class="absolute bottom-6 left-6">
+    <Toast />
+  </div>
 </div>
--- frontend/svelte-app/src/components/feedback/Toast.svelte
+++ frontend/svelte-app/src/components/feedback/Toast.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-
+  import Icon from "@iconify/svelte";
 </script>
 
-<div class="flex">
-    <div class="bg-green-500 text-white p-2 rounded-l">
-        Toast goes here
-    </div>
+<div class="flex bg-card border border-headline text-headline font-bold py-2 px-4 gap-4 rounded-l">
+  <Icon icon="iconamoon:check-fill" width="25" height="25" />
+  Toast message goes here
 </div>
